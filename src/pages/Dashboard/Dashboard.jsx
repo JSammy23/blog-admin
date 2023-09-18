e0bf8b46f8909ed918ecdfe4be85cb1180c5b680
Jordan <jordansampson23@gmail.com>
2023-09-17 21:48:27 -0500

    Init Dashboard & styles.

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { fetchAllPosts } from '../../api';
+import './dashboard.styles.css';
+import Card from '../../components/Card/Card';
+
+const Dashboard = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const postsData = await fetchAllPosts();
+      setPosts(postsData)
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <div className="header">
+        <h1>Blog Admin Portal</h1>
+      </div>
+      <div className="card-container">
+        {posts.map((post) => (
+          <Card post={post} key={post._id} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard;
