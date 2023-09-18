26f4ec7d1f0da6bba67194c5cdd29168fcb9793d
Jordan <jordansampson23@gmail.com>
2023-09-17 21:56:47 -0500

    Format timestamp date-fns.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
-
+import { format, parseISO } from 'date-fns';
 import './Card.styles.css';
 
 
 const Card = ({ post }) => {
+
+  // Format timestamp
+  const timestamp = parseISO(post.timestamp);
+  const formattedDate = format(timestamp, 'MMM do, yyyy');
   return (
     <div className="card">
       <p className="heading">{post.title}</p>
-      <p className="heading">{post.timestamp}</p>
+      <p className="heading">{formattedDate}</p>
     </div>
   )
 }
