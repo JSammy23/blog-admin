6418b2127bc6d512242670abb287600ebddd11ee
Jordan <jordansampson23@gmail.com>
2023-09-22 11:22:35 -0500

    Link compose btn to route.

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { fetchAllPosts } from '../../api';
+import { useNavigate } from 'react-router-dom';
 import './dashboard.styles.css';
 import Card from '../../components/Card/Card';
 import { usePost } from '../../context/postContext';
@@ -7,6 +8,7 @@ import ButtonComponent from '../../components/Button/ButtonComponent';
 
 const Dashboard = () => {
   const { posts, setPosts } = usePost();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getPosts = async () => {
@@ -16,11 +18,15 @@ const Dashboard = () => {
     getPosts();
   }, []);
 
+  const handleComposeClick = () => {
+    navigate('/post/compose');
+  }
+
   return (
     <div>
       <div className="header">
         <h1>Blog Admin Portal</h1>
-        <ButtonComponent size='large' >Compose</ButtonComponent>
+        <ButtonComponent size='large' onClick={handleComposeClick} >Compose</ButtonComponent>
       </div>
       <div className="card-container">
         {posts.map((post) => (
