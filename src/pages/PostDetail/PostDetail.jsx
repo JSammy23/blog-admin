142fd11a86ec2f6591b2fdae04f8485c65f6c917
Jordan <jordansampson23@gmail.com>
2023-09-28 13:03:06 -0500

    Import modal and delete post functionality.

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { usePost } from "../../context/postContext";
 import { useParams } from "react-router-dom";
-import { fetchPostById } from "../../api";
+import { fetchPostById, deletePost } from "../../api";
 import ButtonComponent from "../../components/Button/ButtonComponent";
+import Modal from "../../components/Modal/Modal";
 import { useNavigate } from 'react-router-dom';
+import { format, parseISO } from 'date-fns';
 import './PostDetail.styles.css';
 
 function PostDetail() {
@@ -12,8 +14,16 @@ function PostDetail() {
   const [post, setPost] = useState(getPostById(postId));
   const [loading, setLoading] = useState(!post);
   const [error, setError] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const navigate = useNavigate();
+
+  // Format timestamp
+  let formattedDate;
+  if (post) {
+    const timestamp = parseISO(post.timestamp);
+    formattedDate = format(timestamp, 'MMM do, yyyy');
+  }
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -37,6 +47,11 @@ function PostDetail() {
     navigate(`/post/${postId}/edit`, { state: { post }});
   };
 
+  const handleDeletePost = async () => {
+    await deletePost(post._id);
+    navigate('/home');
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -52,8 +67,15 @@ function PostDetail() {
   return (
     <div>
       <div className="controls">
+        <ButtonComponent onClick={() => setIsModalOpen(true)} >Delete Post</ButtonComponent>
         <ButtonComponent onClick={handleEditClick} >Edit Post</ButtonComponent>
       </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} >
+        <h2>Do you really want to delete this post?</h2>
+        <p><span>Title: </span> {post.title}</p>
+        <p><span>Date: </span> {formattedDate}</p>
+        <button className="btn danger" onClick={handleDeletePost} >Delete</button>
+      </Modal>
       <div className="post-container" >
           <div className="post-header">
             <h3 className="title" >{post.title}</h3>
