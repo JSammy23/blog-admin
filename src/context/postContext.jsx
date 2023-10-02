62a847b9547d8e51d832ff8f29a5d5fb1ac77728
Jordan <jordansampson23@gmail.com>
2023-10-02 15:10:33 -0500

    Remove comment from post, contextValue object.

--- src/context/postContext.jsx
+++ src/context/postContext.jsx
@@ -13,8 +13,35 @@ export const PostProvider = ({ children }) => {
         return posts.find(post => post._id === id);
     };
 
+    const deleteCommentFromPost = (postId, commentId) => {
+        // Find the index of the post with the specified postId
+        const postIndex = posts.findIndex(post => post._id === postId);
+    
+        if (postIndex === -1) {
+            return; // Post not found
+        }
+    
+        // Create a shallow copy of the post
+        const updatedPost = { ...posts[postIndex] };
+    
+        // Filter out the comment with the given commentId
+        updatedPost.comments = updatedPost.comments.filter(comment => comment._id !== commentId);
+    
+        // Update the posts state with the new list of posts
+        const updatedPosts = [...posts];
+        updatedPosts[postIndex] = updatedPost;
+        setPosts(updatedPosts);
+    };
+
+    const contextValue = {
+        posts,
+        setPosts,
+        getPostById,
+        deleteCommentFromPost
+    }
+
     return (
-        <PostContext.Provider value={{ posts, setPosts, getPostById }}>
+        <PostContext.Provider value={contextValue}>
             {children}
         </PostContext.Provider>
     );
